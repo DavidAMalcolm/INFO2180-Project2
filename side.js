c034a3511042884bf50b9da7fcaa4b6a43c543b9
ChildishWu <david.a.malcolms@gmail.com>
2023-12-08 01:57:12 -0500

    logout implemented

--- side.js
+++ side.js
@@ -12,7 +12,6 @@ function userList(event){
 
 function logOut(event){
   event.preventDefault();
-  getSide("");
   logoutUser();
 }
 
@@ -54,9 +53,49 @@ function logoutUser() {
               throw new Error("Logout request failed.");
           }
       })
-      .then(() => {
-          getPage("index.html");
-      })
+      .then((responseText) => {
+          document.getElementsByTagName('html')[0].innerHTML = responseText;
+          
+            let prompt = document.getElementById("prompter");
+        
+            prompt.addEventListener("click",function(event){
+              event.preventDefault();
+              const user = document.getElementById("user").value;
+              const pass = document.getElementById("pass").value;
+        
+            fetch("login.php",{
+                method:"POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+                body: new URLSearchParams({
+                    username: user,
+                    pass: pass,
+                }).toString(),
+            })
+                .then((response)=>response.text())
+                .then((responseText)=>{
+                    let resolution = responseText;
+                    if (resolution.includes("success")){
+                        //something
+                        getPage("add.html");
+                        document.getElementsByTagName('main')[0].style.display = "block";
+                        document.getElementsByTagName('main')[0].style.gridColumn = 2;
+                        document.getElementsByTagName("footer")[0].style.marginTop = "0px";
+                        getSide("sidebar.html");
+                        console.log(resolution);
+                    }
+                    else{
+                        alert("Incorrect User Information");
+                        console.log(resolution);
+                    }
+                })
+                .catch(error=>{
+                    console.error("Error:"+ error);
+                })
+            })
+        })
+          
       .catch(error => {
           console.error("Error: " + error.message);
       });
